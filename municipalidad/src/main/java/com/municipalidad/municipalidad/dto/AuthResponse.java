dde378d035243bd4eea2361e61d74f7efe0d397c
Ing. Anderson F. <fernandezanderson562@gmail.com>
2025-11-25 19:34:06 -0500

    Initial commit - Municipal Digital Platform

---
+++ municipalidad/src/main/java/com/municipalidad/municipalidad/dto/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.municipalidad.municipalidad.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse() {}
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
